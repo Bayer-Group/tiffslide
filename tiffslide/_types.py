286303ad588a3d1c8289de1debaee76eed8d9f05
Andreas Poehlmann <andreas@poehlmann.io>
2021-07-06 13:21:32 +0200

    tiffslide._types: fix minor typing issue

--- tiffslide/_types.py
+++ tiffslide/_types.py
@@ -1,6 +1,11 @@
-import os
+# noinspection PyUnresolvedReferences
+from os import PathLike
 from typing import IO
 from typing import Union
 
+__all__ = [
+    "PathOrFileLike",
+]
+
 # todo: check if this covers all relevant use cases
-PathOrFileLike = Union[str, bytes, os.PathLike, IO[str], IO[bytes]]
+PathOrFileLike = Union[str, bytes, 'PathLike[str]', IO[str], IO[bytes]]
