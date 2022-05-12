4198dd1f527283a218397ab3863e6335a532c5db
Andreas Poehlmann <andreas@poehlmann.io>
2022-05-12 13:24:27 +0200

    benchmark tile reading

---
+++ docs/generate_benchmark_plots.py
@@ -0,0 +1,84 @@
+import json
+import os
+import pathlib
+import subprocess
+import sys
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def run_pytest_benchmarks(force):
+    td = os.environ["OPENSLIDE_TESTDATA_DIR"]
+    assert td, f"must set OPENSLIDE_TESTDATA_DIR. Got: {td!r}"
+    root = pathlib.Path(__file__).parent.parent
+    assert root.joinpath("tiffslide", "tests").is_dir()
+
+    fn_json = ".tiffslide_docs_benchmark.json"
+
+    if not root.joinpath(fn_json).is_file() or force:
+        subprocess.run(
+            ["pytest", "-x", f"--benchmark-json={fn_json}", "--benchmark-only"],
+            env=os.environ,
+            cwd=root,
+            check=True,
+        )
+
+    with root.joinpath(fn_json).open() as f:
+        data = json.load(f)
+
+    records = []
+    for record in data["benchmarks"]:
+        test_name, _, _ = record["name"].partition("[")
+        test_name = test_name[5:]
+        for rtime in record["stats"]["data"]:
+            ps = record["params"].copy()
+            file_type, modname = ps.pop("slide_with_tile_size")
+            records.append({
+                "test_name": test_name,
+                "file_type": file_type,
+                "modname": modname,
+                "label": "-".join(ps.values()),
+                "time": rtime,
+            })
+
+    df = pd.DataFrame.from_records(records)
+    for test_name, ptdf in df.groupby("test_name"):
+
+        ft_ax_order = [
+            "svs",
+            "generic",
+            "hamamatsu",
+            "ventana",
+            "leica",
+        ]
+        assert set(ptdf["file_type"].unique()) == set(ft_ax_order)
+        fig, axes = plt.subplots(1, len(ft_ax_order), figsize=(12, 4))
+        fig.suptitle(test_name)
+        ft_ax_map = {
+            ft: ax
+            for ft, ax in zip(ft_ax_order, axes)
+        }
+
+        for file_type, tdf in ptdf.groupby("file_type"):
+            ldf = tdf.groupby(["modname", "label"]).mean()
+            ldf["time"] *= 1000
+            ldf = ldf.reset_index()
+            pdf = ldf.pivot(index="label", columns="modname", values="time")
+            idx = sorted(pdf.index.tolist())
+            pdf = pdf.loc[idx, :]
+            pdf.plot.barh(title=f"{file_type}", ax=ft_ax_map[file_type], legend=False)
+
+        axes[0].set_ylabel("access pattern")
+        for ax in axes[1:]:
+            ax.set_ylabel("")
+            ax.set_yticklabels([])
+        for ax in axes:
+            ax.set_xlabel("time (ms)")
+
+        fig.savefig(root.joinpath("docs", "images", f"benchmark_{test_name}.png"))
+
+
+if __name__ == "__main__":
+    force = sys.argv[-1] == "--force"
+    run_pytest_benchmarks(force)
---
+++ tiffslide/tests/test_benchmark.py
@@ -0,0 +1,182 @@
+import os
+import platform
+import random
+import subprocess
+import sys
+import warnings
+from itertools import product
+from getpass import getpass
+
+import numpy as np
+import pytest
+import importlib
+
+OPENSLIDE_TESTDATA_DIR = os.getenv("OPENSLIDE_TESTDATA_DIR", None)
+FILES = {
+    "svs": "Aperio/CMU-2.svs",
+    "generic": "Generic-TIFF/CMU-1.tiff",
+    "hamamatsu": "Hamamatsu/OS-3.ndpi",
+    "leica": "Leica/Leica-2.scn",
+    "ventana": "Ventana/OS-2.bif",
+}
+if OPENSLIDE_TESTDATA_DIR is None:
+    pytestmark = pytest.mark.skip
+
+else:
+    for key, fn in FILES.items():
+        FILES[key] = os.path.join(OPENSLIDE_TESTDATA_DIR, fn)
+
+
+MODULES = ["tiffslide", "openslide"]
+
+
+@pytest.fixture(scope="module")
+def root_pw(request):
+    capture = request.config.pluginmanager.getplugin("capturemanager")
+    capture.suspend_global_capture(in_=True)
+    if not sys.stdin.isatty() and sys.stdout.isatty():
+        warnings.warn("DISK CACHE INVALIDATION NEEDS ROOT, but running non-interactive")
+        root_pw = None
+    else:
+        root_pw = getpass("\n\nPLEASE ENTER ROOT PW (empty disables disk cache invalidation): ")
+    capture.resume_global_capture()
+    yield root_pw
+
+
+def try_to_clear_disk_cache(root_pass):
+    env = os.environ.copy()
+    if root_pw:
+        env["SUDO_PASSWORD"] = root_pass
+
+    system = platform.system()
+    if system == "Linux":
+        # from: https://github.com/sdvillal/jagged/blob/6fc83aa11e/jagged/benchmarks/utils.py#L95-L118
+        max_size = "1000G"
+        drop_level = 3
+
+        if 0 != os.system('vmtouch -e -f -q -m %s "%s"' % (max_size, OPENSLIDE_TESTDATA_DIR)):
+            subprocess.run(
+                r"""printf '%%s\n' "$SUDO_PASSWORD" """
+                r"""| sudo -p "" -S -- sh -c 'sync && echo %d > /proc/sys/vm/drop_caches'""" % drop_level,
+                env=env,
+                check=True,
+                shell=True,
+            )
+
+    elif system == "Darwin":
+        subprocess.run(
+            r"""printf '%s\n' "$SUDO_PASSWORD" | sudo -p "" -S -- sh -c 'sync && purge'""",
+            env=env,
+            check=True,
+            shell=True,
+        )
+
+    elif system == "Windows":
+        warnings.warn("not implemented for windows yet")
+
+
+@pytest.fixture(
+    params=[
+        pytest.param((ft, m), id=f"{m}-{ft}")
+        for ft, m in product(FILES, MODULES)
+    ]
+)
+def slide_with_tile_size(request, root_pw):
+    """yield a slide together with the internal tile size"""
+    file_type, module_name = request.param
+    file_name = FILES[file_type]
+
+    # get the corresponding module
+    mod = importlib.import_module(module_name)
+    open_slide = getattr(mod, "open_slide")
+
+    try_to_clear_disk_cache(root_pw)
+
+    slide = open_slide(file_name)
+
+    # attach tile_size
+    try:
+        t_size = (
+            int(slide.properties[f"{module_name}.level[0].tile-width"]),
+            int(slide.properties[f"{module_name}.level[0].tile-height"]),
+        )
+    except KeyError:
+        warnings.warn(
+            "recovering tile_size via tiffslide"
+        )
+        ts_cls = getattr(importlib.import_module("tiffslide"), "TiffSlide")
+        p = ts_cls(file_name).properties
+        t_size = (
+            int(p[f"tiffslide.level[0].tile-width"]),
+            int(p[f"tiffslide.level[0].tile-height"]),
+        )
+
+    try:
+        yield slide, t_size
+    finally:
+        slide.close()
+
+
+def get_locations(slide, order, tsize):
+    def _locations(s, ts, c_order):
+        w, h = s.dimensions
+        tw, th = ts
+
+        out = []
+        if c_order:
+            for y in range(0, h, th):
+                for x in range(0, w, tw):
+                    out.append((x, y))
+        else:
+            for x in range(0, w, tw):
+                for y in range(0, h, th):
+                    out.append((x, y))
+        return out
+
+    if order == "seqrow":
+        locations = _locations(slide, tsize, c_order=True)
+    elif order == "seqcol":
+        locations = _locations(slide, tsize, c_order=False)
+    elif order == "random":
+        locations = _locations(slide, tsize, c_order=True)
+        random.seed(42)
+        random.shuffle(locations)
+    else:
+        raise ValueError(order)
+    return locations
+
+
+@pytest.mark.parametrize("order", ["seqrow", "seqcol", "random"])
+def test_read_tiles_as_pil(order, slide_with_tile_size, benchmark):
+    slide, tsize = slide_with_tile_size
+    locations = get_locations(slide, order, tsize)
+    lociter = iter(locations)
+
+    def setup():
+        return (next(lociter), 0, tsize), {}
+
+    read_tile = slide.read_region
+
+    benchmark.pedantic(read_tile, setup=setup, rounds=64, iterations=1, warmup_rounds=1)
+
+
+@pytest.mark.parametrize("order", ["seqrow", "seqcol", "random"])
+def test_read_tiles_as_numpy(order, slide_with_tile_size, benchmark):
+    slide, tsize = slide_with_tile_size
+    locations = get_locations(slide, order, tsize)
+    lociter = iter(locations)
+
+    def setup():
+        return (next(lociter), 0, tsize), {}
+
+    _read_tile = slide.read_region
+    if slide.__class__.__name__ == "OpenSlide":
+        def read_tile_np(loc, level, size):
+            return np.array(_read_tile(loc, level, size))
+    elif slide.__class__.__name__ == "TiffSlide":
+        def read_tile_np(loc, level, size):
+            return _read_tile(loc, level, size, as_array=True)
+    else:
+        raise NotImplementedError("unknown class")
+
+    benchmark.pedantic(read_tile_np, setup=setup, rounds=64, iterations=1, warmup_rounds=1)
