3004f5e9ed5e458e5c4c9e463f068978b9b5154d
Andreas Poehlmann <andreas@poehlmann.io>
2022-06-28 16:20:16 +0200

    tests: add more files to compatibility testing

--- tiffslide/tests/test_compatibility.py
+++ tiffslide/tests/test_compatibility.py
@@ -4,21 +4,50 @@ import numpy as np
 import pytest
 
 OPENSLIDE_TESTDATA_DIR = os.getenv("OPENSLIDE_TESTDATA_DIR", None)
-FILES = {
-    "svs": "Aperio/CMU-2.svs",
-    "generic": "Generic-TIFF/CMU-1.tiff",
-    "hamamatsu": "Hamamatsu/OS-3.ndpi",
-    "leica": "Leica/Leica-2.scn",
-    "ventana": "Ventana/OS-2.bif",
+_FILES = {
+    "svs": [
+        "Aperio/CMU-1-JP2K-33005.svs",
+        "Aperio/CMU-1-Small-Region.svs",
+        "Aperio/CMU-1.svs",
+        "Aperio/CMU-2.svs",
+        "Aperio/CMU-3.svs",
+        "Aperio/JP2K-33003-1.svs",
+        "Aperio/JP2K-33003-2.svs",
+    ],
+    "generic": [
+        "Generic-TIFF/CMU-1.tiff"
+    ],
+    "hamamatsu": [
+        "Hamamatsu/CMU-1.ndpi",
+        "Hamamatsu/CMU-2.ndpi",
+        "Hamamatsu/CMU-3.ndpi",
+        "Hamamatsu/OS-1.ndpi",
+        "Hamamatsu/OS-2.ndpi",
+        "Hamamatsu/OS-3.ndpi",
+    ],
+    "leica": [
+        "Leica/Leica-1.scn",
+        "Leica/Leica-2.scn",
+        # todo: these two can't be opened by OpenSlide
+        # "Leica/Leica-3.scn",
+        # "Leica/Leica-Fluorescence-1.scn",
+    ],
+    "ventana": [
+        "Ventana/OS-1.bif",
+        "Ventana/OS-2.bif",
+    ],
 }
+FILES = {}
 if OPENSLIDE_TESTDATA_DIR is None:
     pytestmark = pytest.mark.skip
 
 else:
     pytestmark = pytest.mark.compat
 
-    for key, fn in FILES.items():
-        FILES[key] = os.path.join(OPENSLIDE_TESTDATA_DIR, fn)
+    for key, fns in _FILES.items():
+        for fn in fns:
+            k = f"{key}-{fn.split('/')[1]}"
+            FILES[k] = os.path.join(OPENSLIDE_TESTDATA_DIR, fn)
 
 
 MODULES = ["tiffslide", "openslide"]
