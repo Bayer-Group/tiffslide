a6d42359c9bb55151d77d3b2ab907d63c2cfae10
Andreas Poehlmann <andreas@poehlmann.io>
2022-07-15 16:08:52 +0200

    tiffslide._kerchunk: serialize to kerchunk compatible spec

--- tiffslide/_kerchunk.py
+++ tiffslide/_kerchunk.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 import json
+import sys
 from io import StringIO
 from typing import Any
 from typing import TYPE_CHECKING
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
 
 if TYPE_CHECKING:
     from tiffslide.tiffslide import TiffSlide
@@ -14,27 +20,79 @@ __all__ = [
 ]
 
 
+TIFFSLIDE_SPEC_VERSION = 1
 KERCHUNK_SPEC_VERSION = 1
+
+
+class KerchunkSpec(TypedDict):
+    """simple kerchunk version 1 spec"""
+    version: int
+    templates: dict[str, str]
+    gen: list[Any]
+    refs: dict[str, Any]
+
 
 def get_kerchunk_specification(
     slide: TiffSlide,
     *,
     urlpath: str,
-    group_name: str | None = None,
-    level: int | None = None,
-) -> dict[str, Any]:
+    templatename: str | None = None,
+) -> KerchunkSpec:
     """take a tiffslide instance and extract a kerchunk representation"""
     if slide.ts_tifffile.filename is None:
         raise ValueError("can't kerchunk a slide that's not backed by a named file")
 
-    with StringIO() as f:
-        series_idx = slide.properties["tiffslide.series-index"]
-        series = slide.ts_tifffile.series[series_idx]
-        with series.aszarr(level=level) as store:
-            store.write_fsspec(f, urlpath, groupname=group_name, version=KERCHUNK_SPEC_VERSION)
-        kerchunk_dct = json.loads(f.getvalue())
+    kw = {}
+    if templatename is not None:
+        kw["templatename"] = templatename
 
-    return kerchunk_dct
+    combined = KerchunkSpec(
+        version=KERCHUNK_SPEC_VERSION,
+        templates={},
+        gen=[],
+        refs={},
+    )
+    for idx, series in enumerate(slide.ts_tifffile.series):
+        with StringIO() as f, series.aszarr() as store:
+            try:
+                store.write_fsspec(
+                    f,
+                    urlpath,
+                    groupname=f"s{idx}",
+                    version=KERCHUNK_SPEC_VERSION,
+                    **kw,
+                )
+            except ValueError as err:
+                if "incomplete chunks" in str(err):
+                    continue
+                raise
+            kc = json.loads(f.getvalue())
+
+        # combine individual series
+        combined["templates"].update(kc["templates"])
+        combined["gen"].extend(kc["gen"])
+
+        combined_refs = combined["refs"]
+        refs = kc["refs"]
+
+        # insert .zgroup
+        _zgroup = refs.pop(".zgroup")
+        if ".zgroup" in combined_refs:
+            assert combined_refs[".zgroup"] == _zgroup
+        combined_refs[".zgroup"] = _zgroup
+
+        # insert .zattrs
+        if ".zattrs" not in combined_refs:
+            combined_refs[".zattrs"] = json.dumps({
+                "tiffslide.spec_version": TIFFSLIDE_SPEC_VERSION,
+                "tiffslide.properties": slide.properties,
+            })
+        assert ".zattrs" not in refs
+
+        # insert refs
+        combined_refs.update(refs)
+
+    return combined
 
 
 if __name__ == "__main__":
@@ -53,7 +111,7 @@ if __name__ == "__main__":
     parser.add_argument("--storage-options", type=json.loads, help="json encoded storage options", default=None)
     args = parser.parse_args()
 
-    slide = TiffSlide(args.urlpath, storage_options=args.storage_options)
-    kc = get_kerchunk_specification(slide, urlpath=args.urlpath)
+    _slide = TiffSlide(args.urlpath, storage_options=args.storage_options)
+    k = get_kerchunk_specification(_slide, urlpath=args.urlpath)
 
-    pp(kc, compact=True, indent=2)
+    pp(k, compact=True, indent=2)
