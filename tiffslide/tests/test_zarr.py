3b0a6ebdd44af726bbf0b3f7d878543f7273b10c
Andreas Poehlmann <andreas@poehlmann.io>
2022-09-13 17:35:02 +0200

    tests: add test for zarr chunk size

---
+++ tiffslide/tests/test_zarr.py
@@ -0,0 +1,10 @@
+from tiffslide import TiffSlide
+# noinspection PyProtectedMember
+from tiffslide._zarr import get_zarr_chunk_sizes
+
+
+
+def test_get_chunk_sizes(wsi_file):
+    chunk_sizes = get_zarr_chunk_sizes(TiffSlide(wsi_file).zarr_group)
+    assert chunk_sizes.ndim == 3
+    assert chunk_sizes.sum() > 0
