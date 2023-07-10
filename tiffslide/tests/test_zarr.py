a7ac150beae2437e79240efd1658fbe23d2aa68b
Andreas Poehlmann <andreas@poehlmann.io>
2023-07-10 21:11:39 +0200

    tests: check ZarrStore.__getitem__ called only once per key

--- tiffslide/tests/test_zarr.py
+++ tiffslide/tests/test_zarr.py
@@ -1,3 +1,5 @@
+from tifffile import ZarrStore
+
 from tiffslide import TiffSlide
 
 # noinspection PyProtectedMember
@@ -8,3 +10,20 @@ def test_get_chunk_sizes(wsi_file):
     chunk_sizes = get_zarr_chunk_sizes(TiffSlide(wsi_file).zarr_group)
     assert chunk_sizes.ndim in (2, 3)
     assert chunk_sizes.sum() > 0
+
+
+def test_decode_only_once(wsi_file, monkeypatch):
+    ts = TiffSlide(wsi_file)
+
+    called_keys = []
+    _ZarrStore___getitem__ = ZarrStore.__getitem__
+
+    def _cnt_getitem(self, key):
+        nonlocal called_keys
+        called_keys.append(key)
+        return _ZarrStore___getitem__(self, key)
+
+    monkeypatch.setattr(ZarrStore, "__getitem__", _cnt_getitem)
+
+    _ = ts.read_region((0, 0), 0, (1, 1), as_array=True)
+    assert len(called_keys) == len(set(called_keys))
