4a39af2ca3e4341a78fb7d8db466a07252fdb44c
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-01-26 18:40:19 +0100

    limit zarr version

--- tiffslide/_zarr.py
+++ tiffslide/_zarr.py
@@ -17,7 +17,7 @@ from tifffile import TiffFile
 from tifffile import ZarrTiffStore
 from zarr.storage import BaseStore
 from zarr.storage import FSStore
-from zarr.storage import KVStore as _KVStore
+from zarr.storage import KVStore
 
 from tiffslide._compat import NotTiffFile
 from tiffslide._types import Point3D
@@ -38,16 +38,6 @@ __all__ = [
 
 
 # --- zarr storage classes --------------------------------------------
-
-if "__contains__" not in _KVStore.__dict__:
-    # fix missing contains caused double decode:
-    # https://github.com/bayer-science-for-a-better-life/tiffslide/issues/72#issuecomment-1627918238
-    class KVStore(_KVStore):
-        def __contains__(self, item: str) -> bool:
-            return item in self._mutable_mapping
-
-else:
-    KVStore = _KVStore  # type: ignore
 
 
 class _CompositedStore(Mapping[str, Any]):
